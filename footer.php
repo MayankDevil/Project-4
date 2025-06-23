b69c982589d145acede4278bcbfdad7e3b2b116f
MayankDevil <mastermayank91@gmail.com>
2025-06-23 16:56:01 +0530

    add footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer id="footer">
+    <div class="container-fluid bg-dark text-light py-2">
+        <div class="container">
+
+            <div class="h6"> Developed | Designed by <a href="https://mayankdevil.github.io/Mayankdevil" class="text-decoration-none text-info"> Mayank </a> </div>
+        
+        </div>
+    </div>
+</footer>
