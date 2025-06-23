e0294501af93059c9204bc73974c5725bbd28ac0
MayankDevil <mastermayank91@gmail.com>
2025-06-23 16:55:50 +0530

    add header

---
+++ header.php
@@ -0,0 +1,26 @@
+<header class="" id="header">
+
+    <div class="container-fluid bg-dark py-3">
+        <div class="container">
+            <div class="row d-flex align-items-center">
+                <div class="col">
+                    <a href="#" class="brand-name h2 text-light"> Brand Name </a>
+                </div>
+                <div class="col text-end">
+                    <a href="#" class="btn btn-sm btn-outline-info"> Singin </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-black text-secondary">
+        <div class="container">
+            <nav class="d-flex text-start" id="navbar">
+                <a href="#" class="nav-link"> nav_link_name </a>
+                <a href="#" class="nav-link"> nav_link_name </a>
+                <a href="#" class="nav-link"> nav_link_name </a>
+            </nav>
+        </div>
+    </div>
+
+</header>
