9ed1e84dabbbcd9935e28dcb2a4cf3ee2a3350c6
MayankDevil <mastermayank91@gmail.com>
2025-06-23 15:45:35 +0530

    add index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+-   Project: ""
+-   File: frontend/user/app.php
+-   Description: application 
+-->
+<html lang="en">
+
+<head>
+
+    <?php require("main.php"); ?>
+
+</head>
+
+<body>
+
+	<!-- main -->
+	<main id="root">
+		
+		<h1>welcome</h1>
+
+	</main>
+
+</body>
+
+</html>
+<!-- Developer: Mayank Devil | https://mayankdevil.github.io/MayankDevil -->
