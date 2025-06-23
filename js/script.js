95a6d0ba824b7aae59b2dd9727e16243b557c1b9
MayankDevil <mastermayank91@gmail.com>
2025-06-23 15:41:26 +0530

    add js/

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+-   Project: website-5 "Annapurna Bhojnalaya"
+-   File: /frontend/js/script.js
+-   Description: main script
+*/
+
+$(document).ready(function () {
+
+    console.log('script active')
+
+})
+
+/* Developer: Mayank Devil | https://mayankdevil.github.io/MayankDevil */
