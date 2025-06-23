21b8f8499b65a59d1e65c76abbfa2cbf8957c4a9
MayankDevil <mastermayank91@gmail.com>
2025-06-23 15:45:49 +0530

    add main

---
+++ main.php
@@ -0,0 +1,39 @@
+<!--
+-   Project: ""
+-   File: frontend/user/main.php
+-   Description: head section of webpage that contain all metadata script style or assets
+-->
+
+<?php
+
+    # set base url
+
+    define("BASE_URL", "http://localhost/website-5/");
+
+?>
+
+<title>MayankDevil</title>
+
+<!-- metadata -->
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Mayank Devil">
+<meta name="developer" content="https://mayankdevil.github.io/MayankDevil">
+
+<!-- style -->
+
+<link rel="stylesheet" href="<?= BASE_URL?>frontend/assets/bootstrap.min.css">
+<link rel="stylesheet" href="<?= BASE_URL?>frontend/assets/bootstrap-icons.min.css">
+<link rel="stylesheet" href="<?= BASE_URL?>frontend/css/style.css?v=2.1">
+<link rel="stylesheet" href="<?= BASE_URL?>frontend/css/responsive.css?v=2.0">
+
+
+<!-- script -->
+
+<script src="<?= BASE_URL?>frontend/assets/jquery.min.js"></script>
+<script src="<?= BASE_URL?>frontend/assets/bootstrap.bundle.min.js"></script>
+<script src="<?= BASE_URL?>frontend/js/script.js?v=1.0"></script>
+
+<!-- Developer: Mayank Devil | https://mayankdevil.github.io/MayankDevil -->
