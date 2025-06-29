612b3a8c41f0163aeedc611c10ecf29713ff5dd6
MayankDevil <mastermayank91@gmail.com>
2025-06-29 16:48:33 +0530

    update script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 /*
--   Project: website-5 "Annapurna Bhojnalaya"
--   File: /frontend/js/script.js
+-   Project: ""
+-   File: js/script.js
 -   Description: main script
 */
-
 $(document).ready(function () {
 
-    console.log('script active')
+    console.log('script active');
 
+    function activeSection (section) {
+        
+        let clicked = section.text().trim().toLowerCase()
+        
+        $("#post_section, #categorie_section, #user_section").hide();
+
+        if (clicked === "post") {
+            $("#post_section").show();
+        } else if (clicked === "categorie") {
+            $("#categorie_section").show();
+        } else if (clicked === "user") {
+            $("#user_section").show();
+        }
+    }
+
+    /* on click button call activesection pass this */
+
+    let navbar_button = $("#dash_navbar button")
+
+    navbar_button.on("click", function () {
+
+        activeSection($(this))
+    })
+
+    activeSection(navbar_button.eq(1)) // default section
+    
 })
 
 /* Developer: Mayank Devil | https://mayankdevil.github.io/MayankDevil */
