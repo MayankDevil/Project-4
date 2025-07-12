9af4af4e3f54a7eec3c243e7b0dca6dcc96047e5
MayankDevil <mastermayank91@gmail.com>
2025-07-12 18:04:03 +0530

    update users

--- staff/users.php
+++ staff/users.php
@@ -1,7 +1,16 @@
+<!DOCTYPE html>
+<!--
+-   Project: "simriti" Project-4
+-   File: staff/users.php
+-   Description: users all data;
+-->
+
 <?php
 
+session_start();
+
 if (!isset($_SESSION["userename"]) && !isset($_SESSION["user_id"])) {
- 
+
     header("Location:login.php");
     exit;
 }
@@ -9,117 +18,147 @@ if (!isset($_SESSION["userename"]) && !isset($_SESSION["user_id"])) {
 $request_page = isset($_REQUEST['page_num']) ? intval($_REQUEST['page_num']) : 1;
 
 ?>
-<!-- USER SECTION -->
-<section class="collapses" id="user_section">
-    <div class="my-5" id="">
 
-        <div class="w-100 d-flex justify-content-between align-items-center">
-            <h4 class="h4"> USER </h4>
+<html lang="en">
 
-            <a href="register.php" class="btn btn-sm btn-dark"> <span class="bi bi-person-plus-fill m-1"></span> New
-                User </a>
-        </div>
+<head>
 
-        <table class="table w-100 m-3 mx-auto" id="user_data_table">
-            <thead class="">
-                <tr>
-                    <th class=''> ID</th>
-                    <th class=''> Username </th>
-                    <th class=''> First Name </th>
-                    <th class=''> Last Name </th>
-                    <th class=''> Email </th>
-                    <th class=''> Contact </th>
-                    <th class=''> Role </th>
-                    <th class=''> STATUS </th>
-                    <th class=''> EDIT </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+    <?php require("../main.php"); ?>
 
-                $total_users = DB_Modal::countThe('USERS');
+</head>
 
-                $data_count = intval($total_users['TOTAL_USERS']);
+<body class="bgs-white">
+    <!-- main -->
+    <main id="root">
 
-                $pages = 3;
+        <?php include("../header.php"); ?>
 
-                $page_data_count = ceil($data_count / $pages);
+        <div class="container-fluid">
+            <div class="container">
 
-                $control = ($request_page - 1) * $page_data_count;
+                <!-- USER SECTION -->
+                <section class="collapses" id="user_section">
+                    <div class="my-5" id="">
 
-                $output = DB_Modal::get_users($control, $page_data_count);
+                        <div class="w-100 d-flex justify-content-between align-items-center">
+                            <h4 class="h4"> USER </h4>
 
-                if ($output && is_array($output)) {
-                    if (count($output) > 0) {
-                        foreach ($output as $row) {
+                            <a href="register.php" class="btn btn-sm btn-dark"> <span class="bi bi-person-plus-fill m-1"></span> New
+                                User </a>
+                        </div>
 
-                            $lname = empty($row["last_name"]) ? "NULL" : $row['last_name'];
-                            $role = ($row['role']) ? "Admin" : "User";
-                            $status = ($row['isActive']) ? "<a href='grant.php?user_id=" . $row['id'] . "&action=" . $row['isActive'] . "' class='btn btn-sm btn-success'> <span class=' bi bi-check-circle mx-1'></span> ENABLE </a>" : "<a href='grant.php?user_id=" . $row['id'] . "&action=" . $row['isActive'] . "' class='btn btn-sm btn-danger'> <span class=' bi bi-x-circle mx-1'></span> DISABLE </a>";
+                        <table class="table w-100 m-3 mx-auto" id="user_data_table">
+                            <thead class="">
+                                <tr>
+                                    <th class=''> ID</th>
+                                    <th class=''> Username </th>
+                                    <th class=''> First Name </th>
+                                    <th class=''> Last Name </th>
+                                    <th class=''> Email </th>
+                                    <th class=''> Contact </th>
+                                    <th class=''> Role </th>
+                                    <th class=''> STATUS </th>
+                                    <th class=''> EDIT </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
 
-                            echo "<tr class=''>";
-                            echo "<td class=''>" . $row['id'] . "</td>";
-                            echo "<td class=''>" . $row['name'] . "</td>";
-                            echo "<td class=''>" . $row['first_name'] . "</td>";
-                            echo "<td class=''>" . $lname . "</td>";
-                            echo "<td class=''>" . $row['email'] . "</td>";
-                            echo "<td class=''>" . $row['contact_number'] . "</td>";
-                            echo "<td class=''>" . $role . "</td>";
-                            echo "<td class=''>" . $status . "</td>";
-                            echo "<td class=''> <a href='#" . $row['id'] . "' class='btn btn-sm btn-dark bi bi-pencil-square'></a> </td>";
-                            echo "</tr>";
-                        }
-                    } else {
-                        echo "<div class='alert alert-warning w-50 m-3 mx-auto'> user table is empty </div>";
-                    }
-                } else {
-                    echo "<div class='alert alert-danger w-50 m-3 mx-auto'> Query Problem Please checkout! </div>";
-                }
+                                $total_users = DB_Modal::countThe('USERS');
 
-                ?>
-            </tbody>
-        </table>
+                                $data_count = intval($total_users['TOTAL_USERS']);
 
-        <!-- pagination -->
-        <nav id="pagination">
-            <ul class="pagination">
-                <?php
+                                $pages = 3;
 
-                    /* previous page */
-                    if ($request_page > 1) {
+                                $page_data_count = ceil($data_count / $pages);
 
-                        echo "<li class='page-item'>
+                                $control = ($request_page - 1) * $page_data_count;
+
+                                $output = DB_Modal::get_users($control, $page_data_count);
+
+                                if ($output && is_array($output)) {
+                                    if (count($output) > 0) {
+                                        foreach ($output as $row) {
+
+                                            $lname = empty($row["user_lname"]) ? "NULL" : $row['user_lname'];
+                                            $role = ($row['user_role']) ? "Admin" : "User";
+                                            $status = ($row['isOK']) ? "<a href='grant.php?user_id=" . $row['user_id'] . "&action=" . $row['isOK'] . "' class='btn btn-sm btn-success'> <span class=' bi bi-check-circle mx-1'></span> ENABLE </a>" : "<a href='grant.php?user_id=" . $row['user_id'] . "&action=" . $row['isOK'] . "' class='btn btn-sm btn-danger'> <span class=' bi bi-x-circle mx-1'></span> DISABLE </a>";
+
+                                            echo "<tr class=''>";
+                                            echo "<td class=''>" . $row['user_id'] . "</td>";
+                                            echo "<td class=''>" . $row['user_name'] . "</td>";
+                                            echo "<td class=''>" . $row['user_fname'] . "</td>";
+                                            echo "<td class=''>" . $lname . "</td>";
+                                            echo "<td class=''>" . $row['user_mail'] . "</td>";
+                                            echo "<td class=''>" . $row['user_number'] . "</td>";
+                                            echo "<td class=''>" . $role . "</td>";
+                                            echo "<td class=''>" . $status . "</td>";
+                                            echo "<td class=''> <a href='#" . $row['user_id'] . "' class='btn btn-sm btn-outline-dark texts-red borders-red bi bi-pencil-square'></a> </td>";
+                                            echo "</tr>";
+                                        }
+                                    } else {
+                                        echo "<div class='alert alert-warning w-50 m-3 mx-auto'> user table is empty </div>";
+                                    }
+                                } else {
+                                    echo "<div class='alert alert-danger w-50 m-3 mx-auto'> Query Problem Please checkout! </div>";
+                                }
+
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <!-- pagination -->
+                        <nav id="pagination">
+                            <ul class="pagination">
+                                <?php
+
+                                /* previous page */
+                                if ($request_page > 1) {
+
+                                    echo "<li class='page-item'>
                                 <a class='page-link' href='?page_num=" . ($request_page - 1) . "' aria-label='Previous'>
                                     <span class='bi bi-chevron-double-left'></span>
                                 </a>
                             </li>";
-                    }
+                                }
 
-                    /* numbers page */
+                                /* numbers page */
 
-                    for ($i = 1; $i <= $pages; $i++) {
+                                for ($i = 1; $i <= $pages; $i++) {
 
-                        if ($request_page == $i) {
-                            echo "<li class='page-item active'><a class='page-link' href='?page_num=$i'>$i</a></li>";
-                        } else {
-                            echo "<li class='page-item'><a class='page-link' href='?page_num=$i'>$i</a></li>";
-                        }
-                    }
+                                    if ($request_page == $i) {
+                                        echo "<li class='page-item active'><a class='page-link' href='?page_num=$i'>$i</a></li>";
+                                    } else {
+                                        echo "<li class='page-item'><a class='page-link' href='?page_num=$i'>$i</a></li>";
+                                    }
+                                }
 
-                    /* next page */
+                                /* next page */
 
-                    if ($request_page < $pages) {
+                                if ($request_page < $pages) {
 
-                        echo "<li class='page-item'>
+                                    echo "<li class='page-item'>
                                 <a class='page-link' href='?page_num=" . ($request_page + 1) . "' aria-label='Next'>
                                     <span class='bi bi-chevron-double-right'></span>
                                 </a>
                             </li>";
-                    }
-                    
-                ?>
-            </ul>
-        </nav>
+                                }
 
-    </div>
-</section>
+                                ?>
+                            </ul>
+                        </nav>
+
+                    </div>
+                </section>
+
+            </div>
+        </div>
+
+        <?php include("../footer.php"); ?>
+
+    </main>
+
+</body>
+
+</html>
+<!-- Developer: Mayank Devil | https://mayankdevil.github.io/MayankDevil -->
