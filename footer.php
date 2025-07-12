1ee97fe9e0d08b8627dc7517351a3d1ca0091e71
MayankDevil <mastermayank91@gmail.com>
2025-07-12 18:01:27 +0530

    update footer

--- footer.php
+++ footer.php
@@ -1,8 +1,28 @@
-<footer id="footer">
-    <div class="container-fluid bg-dark text-light py-2">
+<footer class="" id="footer">
+    <div class="container-fluid bg-hex text-light py-5">
         <div class="container">
 
-            <div class="h6"> Developed | Designed by <a href="https://mayankdevil.github.io/Mayankdevil" class="text-decoration-none text-info"> Mayank </a> </div>
+            <div class="row">
+
+                <div class="col-6 py-3">
+
+
+                    <a href="https://mayankdevil.github.io/Indardanus" class="btn btn-sm btn-light"> Indardanus </a>
+
+                    <a href="https://mayankdevil.github.io/EntityCode" class="btn btn-sm btn-light"> Entity Code </a>
+
+                    <a href="https://mayankdevil.github.io/ProCoders" class="btn btn-sm btn-light"> ProCodes </a>
+
+                    <a href="https://mayankdevil.github.io/Project-2" class="btn btn-sm btn-light"> GUPT </a>
+
+                </div>
+
+                <div class="col-12">
+
+                    <h6 class="h6"> Developed | Designed by <a href="https://mayankdevil.github.io/Mayankdevil" target="_blank" title="MayankDevil" class="text-decoration-none text-light lead px-2"> Mayank </a> </h6>
+                </div>
+            </div>
+
         
         </div>
     </div>
